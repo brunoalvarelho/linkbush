6cc1c20b9b713afb0520a533649047ada7b256e2
brunoalvarelho <bruno.alvarelho.92@gmail.com>
2022-07-18 18:39:33 +0200

    update devise logic add home page

--- app/controllers/authentication/user/registrations_controller.rb
+++ app/controllers/authentication/user/registrations_controller.rb
@@ -5,30 +5,15 @@ class Authentication::User::RegistrationsController < Devise::RegistrationsContr
 
   layout "authentication" 
 
-  # GET /resource/sign_up
-  def new
-    super
-  end
-
-  # POST /resource
-  def create
-    super
-  end
-
   protected
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up) do |user|
-      user.permit(
-        :user_name,
-        :email,
-        :password,
-        :accepted_terms_and_conditions
-      )
+      user.permit(:user_name, :email, :password, :accepted_terms_and_conditions)
     end
   end
 
   def after_sign_up_path_for(resource)
-    root_path(resource)
+    root_path
   end
 end
